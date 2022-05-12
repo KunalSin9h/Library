97318d067f2d9696d9789a02b9bcb2638853cdf2
Kunal Singh <kunalsin9h@gmail.com>
2022-05-12 13:02:37 +0530

    [fix] indentation in bit.h and bit2d.h

--- Library/bit.h
+++ Library/bit.h
@@ -1,24 +1,24 @@
 struct Fenwick{
-std::vector<int> T;
-int N;
-Fenwick(int n):N{n}{T.assign(N, 0);}
-Fenwick(std::vector<int> &A): Fenwick((int) A.size()){
-    for(int i = 0; i < N; ++i) add(i, A[i]);
-}
-void add(int i, int x){
-    assert(i < N);
-    for(; i < N; i = (i | (i + 1))) T[i] += x;
-}
-int sum(int r){
-    assert(r < N);
-    int res = 0;
-    for(; r >= 0; r = (r&(r+1)) - 1) res += T[r];
-    return res;
-}
-int sum(int l, int r){
-    return sum(r) - sum(l-1);
-}
-int get_element(int i){
-    return sum(i, i);
-}
+    std::vector<int> T;
+    int N;
+    Fenwick(int n):N{n}{T.assign(N, 0);}
+    Fenwick(std::vector<int> &A): Fenwick((int) A.size()){
+        for(int i = 0; i < N; ++i) add(i, A[i]);
+    }
+    void add(int i, int x){
+        assert(i < N);
+        for(; i < N; i = (i | (i + 1))) T[i] += x;
+    }
+    int sum(int r){
+        assert(r < N);
+        int res = 0;
+        for(; r >= 0; r = (r&(r+1)) - 1) res += T[r];
+        return res;
+    }
+    int sum(int l, int r){
+        return sum(r) - sum(l-1);
+    }
+    int get_element(int i){
+        return sum(i, i);
+    }
 };
---
+++ Library/dsu.h
@@ -0,0 +1,29 @@
+struct Dsu{
+    std::vector<int> parent, rank, size;
+    Dsu(int n){ 
+        parent.resize(n, 0);
+        rank.resize(n, 0);
+        size.resize(n, 1);
+        for(int i = 0;i<n; ++i){parent[i] = i;}
+    }
+    int find(int x){
+        if(parent[x] == x)return x;
+        else return  parent[x] =  find(parent[x]);
+    }
+    void unite(int x, int y){
+        x = find(x), y = find(y);
+        if(x == y)return;
+        if(rank[x] < rank[y]){
+            parent[x] = y;
+            size[y] += size[x];
+        }
+        else{
+            parent[y] = x;
+            size[x] += size[y];
+            if(rank[x] == rank[y])rank[x]++;
+        }
+    }
+    bool same(int x, int y){
+        return (find(x) == find(y));
+    } 
+};

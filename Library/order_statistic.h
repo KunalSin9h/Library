0a391de694ec6904e2d807eb3d16394e33259d25
Kunal Singh <kunalsin9h@gmail.com>
2022-05-18 09:34:55 +0530

    [add] order_statictic.h and splitmix.h

---
+++ Library/order_statistic.h
@@ -0,0 +1,7 @@
+#include "bits/extc++.h"
+
+template<typename Key, typename Val, typename cmp = std::less<Key>>
+using ordered_map = __gnu_pbds::tree<Key, Val, cmp, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+
+template<typename Key, typename cmp = std::less<Key>>
+using ordered_set = ordered_map<Key, __gnu_pbds::null_type, cmp>;
